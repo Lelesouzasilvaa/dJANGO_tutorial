b41e28c5685660ea3d4cb63465e2d5b632b26ce8
Leticia de Souza Silva <86673395+Lelesouzasilvaa@users.noreply.github.com>
2025-03-27 20:06:33 -0300

    27/03/25

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
 from .models import Question
+from django.shortcuts import get_object_or_404, render
+from django.http import Http404
 
 
 def index(request):
@@ -20,3 +21,14 @@ def results(request, question_id):
 
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
+
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, "polls/detail.html", {"question": question})
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
